dd423aa3a77a81d197a29b850de2143953d51430
lukasepple <git@lukasepple.de>
2015-03-18 16:25:52 +0100

    added deployment script

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+TMP=$(mktemp -d)
+
+cd $(dirname "$0")
+
+./site clean || exit 1
+./site build || exit 1
+
+cp -R ./_site/* "$TMP" || exit 1
+
+./site clean || exit 1
+
+git checkout gh-pages || exit 1
+
+
+cp -R "$TMP/"* ./ || exit 1
+
+git add --all | exit 1
+
+git commit -m "updated website output" || exit 1
+
+git checkout master
+
+rm -rf "$TMP" || exit 1
