3f1911e75163a93ee9a54b14df99f22d0e1d2171
Tim Baumann <tim@timbaumann.info>
2015-03-20 00:26:34 +0100

    Erster Versuch mit Travis

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+ORIGIN=${1:origin}
+
 TMP=$(mktemp -d)
 
 cd $(dirname "$0")
@@ -14,12 +16,13 @@ cp -R ./_site/* "$TMP"
 ./site clean
 
 git checkout gh-pages
+git pull "$ORIGIN" gh-pages
 
 cp -R "$TMP/"* ./
 
 git add --all
-git commit -m "updated website output"
-git push origin gh-pages
+git commit -m "updated website output $(date '+%m/%d/%y %H:%M')"
+git push "$ORIGIN" gh-pages
 git checkout master
 
 rm -rf "$TMP"
