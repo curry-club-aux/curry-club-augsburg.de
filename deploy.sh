485d09fb8f32041a0fed41d5a00578d0f88b5f7e
Tim Baumann <tim@timbaumann.info>
2015-03-19 22:59:04 +0100

    deploy.sh: Use 'set -e'

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
+
+set -e
+
 TMP=$(mktemp -d)
 
 cd $(dirname "$0")
 
-./site clean || exit 1
-./site build || exit 1
+./site clean
+./site build
 
-cp -R ./_site/* "$TMP" || exit 1
+cp -R ./_site/* "$TMP"
 
-./site clean || exit 1
+./site clean
 
-git checkout gh-pages || exit 1
+git checkout gh-pages
 
+cp -R "$TMP/"* ./
 
-cp -R "$TMP/"* ./ || exit 1
+git add --all
+git commit -m "updated website output"
+git push origin gh-pages
+git checkout master
 
-git add --all || exit 1
-
-git commit -m "updated website output" || exit 1
-
-git push origin gh-pages || exit 1
-
-git checkout master || exit 1
-
-rm -rf "$TMP" || exit 1
+rm -rf "$TMP"
