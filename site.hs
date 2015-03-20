03eeb644949865d61f5be72d6f7b449e159a91b4
Tim Baumann <tim@timbaumann.info>
2015-03-20 15:18:22 +0100

    Doofer Fehler

--- site.hs
+++ site.hs
@@ -7,7 +7,7 @@ import           Hakyll
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
-    match "CNAME" $
+    match "CNAME" $ do
         route   idRoute
         compile copyFileCompiler
 
