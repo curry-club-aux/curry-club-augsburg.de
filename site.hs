53970ed757c7ec4d712be3e6ae611dc8b5f71d96
Tim Baumann <tim@timbaumann.info>
2015-03-20 14:54:30 +0100

    CNAME-Datei für GitHub Pages

--- site.hs
+++ site.hs
@@ -7,6 +7,10 @@ import           Hakyll
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
+    match "CNAME" $
+        route   idRoute
+        compile copyFileCompiler
+
     match "images/*" $ do
         route   idRoute
         compile copyFileCompiler
