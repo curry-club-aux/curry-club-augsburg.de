4d55ccfaf0b66e49228be39b93d522fd21078f20
Profpatsch <mail@profpatsch.de>
2015-04-15 13:48:42 +0000

    Don’t duplicate default.nix

--- shell.nix
+++ shell.nix
@@ -1,14 +1,3 @@
 with (import <nixpkgs> {}).pkgs;
-let pkg = haskellngPackages.callPackage
-            ({ mkDerivation, base, clay, directory, hakyll, stdenv, text }:
-             mkDerivation {
-               pname = "curry-club-augsburg-de";
-               version = "0.1.0.0";
-               src = ./.;
-               isLibrary = false;
-               isExecutable = true;
-               buildDepends = [ base clay directory hakyll text ];
-               license = "unknown";
-             }) {};
-in
-  pkg.env
+let pkg = haskellngPackages.callPackage (import ./default.nix) {};
+in pkg.env
