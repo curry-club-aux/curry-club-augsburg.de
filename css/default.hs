b727426a89fe8d6ba63905c3059cadbfdb147eed
Tim Baumann <tim@timbaumann.info>
2015-04-19 16:22:25 +0200

    Ein ganz neues dunkles Gewand für die Webseite

--- css/default.hs
+++ css/default.hs
@@ -4,72 +4,109 @@ import           Data.Monoid
 import qualified Data.Text.Lazy.IO as L
 import           Clay
 
+reallyDarkPurple, darkerPurple, darkPurple, ourPurple, rose :: Color
+reallyDarkPurple = other "#1e1420"
+darkerPurple = other "#211024"
+darkPurple = other "#2d1630"
+ourPurple = other "#452d49"
+rose = other "#be83c6"
+
 bodyCss :: Css
 bodyCss = do
   body ? do
-    color black
-    fontSize (px 16)
+    color white
+    backgroundColor darkPurple
+    background $ linearGradient (angular $ deg 180)
+      [ (reallyDarkPurple, 0)
+      , (darkerPurple, 5)
+      , (darkPurple, 30)
+      ]
+    --backgroundRepeat noRepeat
+    backgroundAttachment attachFixed
+    fontSize (px 15)
+    lineHeight (px 24)
+    fontWeight lighter
     "font-family" -: "Ubuntu, sans-serif"
     sym margin nil
     width (other "100%")
-    h1 <> h2 <> h3 ? margin (em 1) nil (em 0.5) nil
-    h1 ? fontSize (px 24)
-    h1 ? fontSize (px 20)
-  ((a # link) <> (a # visited)) ? 
-    color (other "#75f")
-  div # "#map" ? do
-    height (px 180)
-    sym margin (px 15)
 
-headerCss :: Css
-headerCss = do
-  (header <> main_ <> footer) ? do
+layoutCss :: Css
+layoutCss = do
+  (header <> main_ <> (footer # ".footer")) ? do
     width (px 600)
     sym2 margin nil auto
   header ? do
-    borderBottom solid (px 2) black
+    marginTop (px 10)
     marginBottom (px 30)
     sym2 padding (px 12) nil
-    nav ? do
+    position relative
+    div # "#logo" ? do
+      width (px 320)
+      float floatLeft
+    blockquote ? do
+      sym margin nil
+      paddingRight (px 40)
       textAlign end
-      ((a # link) <> (a # visited)) ? do
-        color black
-        fontSize (px 18)
-        fontWeight bold
-        marginLeft (px 12)
-        textDecoration none
-  div # "#logo" ? a ? do
-    color black
-    float floatLeft
-    fontSize (px 18)
-    fontWeight bold
-    textDecoration none
-
-footerCss :: Css
-footerCss = do
-  footer ? do
-    borderTop solid (px 2) black
-    color (other "#555")
+      width (px 150)
+      float floatRight
+      p ? sym margin nil
+      footer ? do
+        color rose
+        borderTopStyle none
+        marginTop nil
+        paddingTop (px 6)
+        marginLeft (em 2)
+  -- based on http://www.cssstickyfooter.com
+  div # ".wrap" ? do
+    minHeight (other "100%")
+  div # ".main" ? do
+    overflow auto
+    paddingBottom (px 60)
+  html <> body ? height (other "100%")
+  footer # ".footer" ? do
+    borderTop dashed (px 1) rose
+    color rose
     fontSize (px 12)
-    marginTop (px 30)
-    sym2 padding (px 12) nil
+    position relative
+    marginTop (px (-45))
+    sym2 padding (px 10) nil
     textAlign end
 
-  blockquote |> footer ? do
-    borderTopStyle none
-    marginTop nil
-    paddingTop (px 10)
-    marginLeft (em 2)
-    textAlign start
-
+contentCss :: Css
+contentCss = do
+  div # ".clear" ? do
+    clear both
+  h1 <> h2 <> h3 ? do
+    margin (em 1) nil (em 0.5) nil
+    fontWeight normal
+  --h1 ? fontSize (px 24)
+  h2 ? do
+    fontSize (px 20)
+  "h2:before" ? do
+    fontWeight bold
+    content (stringContent "λ")
+    paddingRight (px 10)
+    color ourPurple
+  ((a # link) <> (a # visited)) ?
+    color rose
+  p # "#next-meeting" ? do
+    fontSize (px 18)
+    textAlign (alignSide sideCenter)
+  address ?
+    float floatLeft
+  div # "#map" ? do
+    float floatRight
+    height (px 220)
+    width (px 400)
+    border solid (px 5) ourPurple
+    --sym2 margin (px 15) nil
   div # "#info" ? do
     color (other "#555")
     fontSize (px 14)
     fontStyle italic
-
 main :: IO ()
 main = L.putStr $ render $ do
   importUrl "/css/ubuntu.css"
   bodyCss
-  headerCss
-  footerCss
+  layoutCss
+  contentCss
