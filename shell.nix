e3907b358593cc8d81ec21c55408467ca541bbd5
Profpatsch <mail@profpatsch.de>
2015-05-05 18:15:11 +0200

    Trying to get it to build with ghc-7.10.

--- shell.nix
+++ shell.nix
@@ -1,2 +1,15 @@
-with (import <nixpkgs> {}).pkgs.haskellngPackages;
-(callPackage (import ./default.nix) { extraDeps = [ cabal-install ]; }).env
+let 
+nixpkgs = import <nixpkgs> {};
+haskellNg = nixpkgs.pkgs.haskell-ng;
+# hopefully temporary, but ghc 7.8 fails because of non-deterministic builds (of GHC)
+ghc710 = haskellNg.packages.ghc7101;
+# Fix for unnecessary patches 7.10 wants to apply see https://github.com/NixOS/nixpkgs/commit/f65fed20deea43096528f87cc94ef6943adc0b37
+ownHakyll = with nixpkgs.pkgs.haskellPackages; callPackage (import ./hakyll-tmp.nix) {};
+pkg = with ghc710; callPackage ./. { hakyll = ownHakyll; };
+ghc = ghc710.ghcWithPackages (pkgs: with pkgs; [ base clay directory ownHakyll text system-filepath ]);
+
+in
+with nixpkgs.lib;
+overrideDerivation pkg (super: {
+  buildInputs = super.buildInputs ++ [ ghc ghc710.cabal-install];
+})
