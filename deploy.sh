1a8020ae0f354bba9e8cabdeabc33fbd7d303aa4
Ingo Blechschmidt <iblech@web.de>
2016-04-24 13:56:57 +0200

    Nächster Versuch

--- deploy.sh
+++ deploy.sh
@@ -24,10 +24,8 @@ ORIGIN=${1:-$CURR_ORIGIN}
 BUILD_DIR=$(mktemp -d builddir-XXXX)
 
 function onerr() {
-  ls -lR
-  ls -lR "$BUILD_DIR"
-  hash stack
-  stack exec runghc css/default.hs
+  ls -laR /home/travis
+  css
   rm -rf "$BUILD_DIR"
   exit 1
 }
