3b37678ad4ebf9524d786cc90ab4da2676cca8f6
Profpatsch <mail@profpatsch.de>
2019-09-08 15:53:06 +0200

    default.nix: fix nixpkgs to 19.09
    TODO: update release.nix

--- default.nix
+++ default.nix
@@ -1,7 +1,14 @@
 let
-  nixpkgs = import <nixpkgs> {};
+  # 2019-09-08 (19.09 release branch)
+  rev = "0d79decdb3ba7da459aafb002745a10bb97141bb";
+  nixpkgs = import (fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+    sha256 = "0rjwmy778mzp845lap10vfrshm3llhavri889qq7khn58wxvazq5";
+  }) {};
   hp = nixpkgs.haskellPackages.override {
     overrides = self: super: {
+      clay = nixpkgs.haskell.lib.overrideCabal super.clay
+        (drv: { broken = false; jailbreak = true; });
       site = super.callPackage ./site.nix {};
       siteEnv = with self;
         (nixpkgs.haskell.lib.addBuildTools self.site
