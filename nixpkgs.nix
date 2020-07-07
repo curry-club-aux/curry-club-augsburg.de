fd88162e397bb26a0281bb1caa95d882eeaed8e8
Tim J. Baumann <tim@timbaumann.info>
2020-07-07 15:17:50 +0200

    Update nixpkgs

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,6 +1,8 @@
-# 2019-09-08 (19.09 release branch)
-let rev = "0d79decdb3ba7da459aafb002745a10bb97141bb";
-in import (fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-  sha256 = "0rjwmy778mzp845lap10vfrshm3llhavri889qq7khn58wxvazq5";
-}) {}
+let
+  srcDef = builtins.fromJSON (builtins.readFile ./nixpkgs.json);
+  nixpkgs = builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/${srcDef.rev}.tar.gz";
+    sha256 = srcDef.sha256;
+  };
+in
+import nixpkgs
